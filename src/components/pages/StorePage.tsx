c26b54145fd0fcd31152ae34fb948ed848ed4038
Leo <leogc337@gmail.com>
2025-12-12 17:23:05 +0300

    Создана строка с элементами управления приложением

--- src/components/pages/StorePage.tsx
+++ src/components/pages/StorePage.tsx
@@ -4,6 +4,28 @@ const StorePage = () => {
 
     return <div className="page-content">
 
+        <div className="store-control-row">
+            <div className="switch-block">
+                <label htmlFor="all" className="switch-options"><input type="radio" name="platform" id="all"/>Все</label>
+                <label htmlFor="steam" className="switch-options"><input type="radio" name="platform" id="steam"/>Steam</label>
+                <label htmlFor="ps" className="switch-options"><input type="radio" name="platform" id="ps"/>PS5</label>
+                <label htmlFor="xbox" className="switch-options"><input type="radio" name="platform" id="xbox"/>Xbox</label>
+                <label htmlFor="switch" className="switch-options"><input type="radio" name="platform" id="switch"/>Switch</label>
+                <label htmlFor="mobile" className="switch-options"><input type="radio" name="platform" id="mobile"/>Мобильные</label>
+            </div>
+
+            <input type="text" placeholder="Поиск игры..." className="search-bar"/>
+
+            <button>Корзина</button>
+
+            <button>Товары</button>
+
+            <div className="switch-block">
+                <label htmlFor="light" className="switch-options"><input type="radio" name="theme" id="light"/>🌞</label>
+                <label htmlFor="dark" className="switch-options"><input type="radio" name="theme" id="dark"/>🌑</label>
+            </div>
+        </div>
+
     </div>
 }
 
