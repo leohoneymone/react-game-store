b48d42189920e61d05497c2c9af2f87d801dd1e8
Leo <leogc337@gmail.com>
2025-12-12 13:08:52 +0300

    Добавлен футер (требует доработок)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "./components/layout/Header";
 import Main from "./components/layout/Main";
+import Footer from "./components/layout/Footer";
 
 const App = () => {
     
     return <>
         <Header />
         <Main />
+        <Footer />
     </>
 }
 
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const year: string = new Date().getFullYear().toString();
+
+const Footer = () => {
+
+    return <footer>
+        <div className="footer-content">
+
+            <div className="footer-links-block">
+                <h3 className="footer-title">Ссылки</h3>
+                <a href="#">GitHub</a>
+                <a href="#">Репозиторий</a>
+                <a href="#">RAWG API</a>                
+            </div>
+
+            <div className="footer-links-block">
+                <h3 className="footer-title">Дополнительно</h3>
+                <a href="#">О CLD GAMES</a>
+                <a href="#">Ценообразование</a>
+                <a href="#">Политика приватности</a>
+            </div>
+
+            <div className="footer-links-block">
+                <h3 className="footer-title">Информация</h3>
+                <a href="#">Поставщикам</a>
+                <a href="#">Покупателям</a>
+                <a href="#">Партнёрам</a>
+            </div>
+
+            <div className="footer-links-block" style={{flexGrow: 1}}>
+                <h3 className="footer-title"> Поддержка</h3>
+                <a href="#">FAQ</a>
+                <a href="#">Электронная почта</a>
+                <a href="#">Направить тикет</a>
+            </div>
+            
+            <div className="author">
+                <p>Автор: Leonard Honeymone</p>
+            </div>
+        </div>
+
+        <div className="contrib-line">
+            <div className="background"></div>
+
+            <div className="contribution">
+                <div className="contribution-content">
+                    <p className="year-info">© {year} - cozamaLeonard. Все права защищены</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+}
+
+export default Footer;
