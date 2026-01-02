390a22f8335fb601a3db6994627991713731b38d
Leo <leogc337@gmail.com>
2026-01-02 16:29:19 +0300

    Добавлена кнопка прокрутки наверх

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
 import { useStoreContext } from "../../utils/context";
 import Toast from "./Toast";
@@ -7,14 +7,32 @@ const Main = ({children}) => {
 
     const {theme, toast} = useStoreContext();
 
+    const [scroll, setScroll] = useState<number>(0);
+
     // Установка темы
     useLayoutEffect(() => {
         document.querySelector('html')?.setAttribute("theme", theme);
-    }, [theme])
+    }, [theme]);
+
+    // Работа со скроллом
+    const handleScroll = ():void => {
+        setScroll(window.scrollY);
+    }
+
+    // Установка обработчика на скролл
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, []);
 
     return <main>
         {children}
         {toast ? <Toast /> : null}
+
+        {scroll > 300 ? <button className="scroll-top" onClick={() => {window.scrollTo(0,0)}}>^</button> : null}
     </main>
 }
 
