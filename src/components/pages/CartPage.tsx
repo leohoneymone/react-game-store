f895e74e96ffbae7414f7cc400d609c97e9f7b82
Leo <leogc337@gmail.com>
2026-01-02 11:46:20 +0300

    Добавлены элементы управления для корзины

--- src/components/pages/CartPage.tsx
+++ src/components/pages/CartPage.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStoreContext } from "../../utils/context";
 import { useNavigate } from "react-router-dom";
+
 import Breadcrumbs from "../common/Breadcrumbs";
 import ThemeToggler from "../common/ThemeToggler";
 import CartItem from "../common/CartItem";
+import Popup from "../layout/Popup";
 
 const CartPage = () => {
 
     // Контекст
     const {setToast, cart, setCart} = useStoreContext();
+    const [orderPopup, setOrderPopup] = useState<boolean>(false);
 
     // Навигация
     const nav = useNavigate();
+
+    // Очистка корзины
+    const handleClearCart = () => {
+        setCart([]);
+        setToast('Корзина успешно очищена');
+    }
 
     return <div className="page-content">
         
@@ -35,11 +44,36 @@ const CartPage = () => {
                     
             <div className="content-row cart-row">
 
-                {cart.map(item => <CartItem key={`${item.cartslug}`} {...item} />)}
+                <div className="cart-items">
+                    {cart.map(item => <CartItem key={item.cartslug} {...item} />)}
+                </div>
+
+                <div className="cart-control-menu">
+                    <h3>Товаров в корзине: {cart.length}</h3>
+
+                    <div className="cart-control-buttons">
+                        <button className="order-btn" onClick={() => {setOrderPopup(true)}}>Оформить заказ</button>
+                        <button className="clear-cart-btn" onClick={() => handleClearCart()}>Очистить корзину</button>
+                    </div>
+                </div>
 
             </div>
 
         </div>
+
+        {orderPopup ? <Popup close={() => {setOrderPopup(false); setCart([]);}}>
+            <h3>Информация о заказе</h3>
+            
+            <div className="order-info">
+                <h1>Заказ успешно оформлен</h1>
+                <ul>
+                    {cart.map(item => <li key={`${item.cartslug}-li`}>{item.name} ({item.platforms})</li>)}
+                </ul>
+                <h3>Ваша заявка оставлена. В течение 3-5 рабочих дней с Вами свяжутся для получения подробностей, а также продолжения работы с заказом (не свяжутся)</h3>
+                <button onClick={() => {setOrderPopup(false); setCart([]);}}>ОК</button>
+            </div>
+
+        </Popup> : null}
         
     </div>
 }
